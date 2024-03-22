a365ef1fe3e6e993e404ba6edcfc1920a76eaca9
VoiLuisa <108806605+LuisaVoicu@users.noreply.github.com>
2024-03-22 10:40:01 +0200

    removed zip

---
+++ petshop/petshop/src/test/java/com/petshop/petshop/PetShopTest.java
@@ -0,0 +1,19 @@
+package com.petshop.petshop;
+
+import com.petshop.petshop.repository.PetRepository;
+import com.petshop.petshop.repository.ProductRepository;
+import com.petshop.petshop.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PetShopTest {
+
+    private static UserRepository userRepository;
+    private static PetRepository petRepository;
+    private static ProductRepository productRepository;
+
+
+}
